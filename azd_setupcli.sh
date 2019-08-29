2f6a79a2db7e07cccb458e8ff5d07bf48dce289d
Sudhakar K R <sudhakarkr@LP-C02SY4XVGTFM.local>
2019-08-29 11:01:28 +0200

    Added azd Release Bash Script to Intall istio and helm

---
+++ azd_setupcli.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+echo "Login into AKS"
+az aks get-credentials -n istioSampleCluster -g istioSampleRG
+
+echo "Setup tiller for Helm, we will discuss about this tool later"
+kubectl create serviceaccount tiller --namespace kube-system
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+
+ISTIO_VERSION=1.2.5
+
+curl -L https://git.io/getLatestIstio | ISTIO_VERSION=1.2.5 sh -
+
+cd istio-$ISTIO_VERSION
+sudo cp ./bin/istioctl /usr/local/bin/istioctl
+sudo chmod +x /usr/local/bin/istioctl
+
+helm install install/kubernetes/helm/istio-init --name istio-init --namespace istio-system
+
+kubectl get jobs -n istio-system
+
+GRAFANA_USERNAME=$(echo -n "grafana" | base64)
+GRAFANA_PASSPHRASE=$(echo -n "grAfAnA" | base64)
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: grafana
+  namespace: istio-system
+  labels:
+    app: grafana
+type: Opaque
+data:
+  username: $GRAFANA_USERNAME
+  passphrase: $GRAFANA_PASSPHRASE
+EOF
+
+KIALI_USERNAME=$(echo -n "kiali" | base64)
+KIALI_PASSPHRASE=$(echo -n "kIalI" | base64)
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kiali
+  namespace: istio-system
+  labels:
+    app: kiali
+type: Opaque
+data:
+  username: $KIALI_USERNAME
+  passphrase: $KIALI_PASSPHRASE
+EOF
+
+helm install install/kubernetes/helm/istio --name istio --namespace istio-system \
+  --set global.controlPlaneSecurityEnabled=true \
+  --set mixer.adapters.useAdapterCRDs=false \
+  --set grafana.enabled=true --set grafana.security.enabled=true \
+  --set tracing.enabled=true \
+  --set kiali.enabled=true
+
